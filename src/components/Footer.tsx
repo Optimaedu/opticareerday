6b0f141c27cc3a816387dc156f709e3864e16ec5
Oliver <reeidcsgo@gmail.com>
2022-12-19 12:43:58 +0200

    import react remove

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import React from 'react';
 import FacebookIcon from './ui/icons/FacebookIcon';
 import InstagramIcon from './ui/icons/InstagramIcon';
 import LinkedInIcon from './ui/icons/LinkedInIcon';
